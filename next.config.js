1d848a0796e179632d897ee1120871c410a6ea47
Emiliano Di Rosa Amores <emilianodirosa1@gmail.com>
2026-09-01 19:56:27 +0200

    Add image domain and rewrites to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     domains: ['images.unsplash.com'],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/lab',
+        destination: 'https://kamelo.vercel.app/lab',
+      },
+      {
+        source: '/lab/:path*',
+        destination: 'https://kamelo.vercel.app/lab/:path*',
+      },
+    ];
   },
 };
 
